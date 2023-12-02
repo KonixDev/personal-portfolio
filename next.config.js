c67c088c9e237cea3d50b0b6112fad0626a217e7
Martin <martincoll000@gmail.com>
2023-12-02 03:17:50 -0300

    fix npm run export deprecated next js 14

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     // here you can add the url's that you are planning
     // to use inside your next/image.
