84d033761d8f05b22b8c628e92a65722a5819e82
Martin <martincoll000@gmail.com>
2023-12-05 16:55:57 -0300

    new projects

--- data/default/projects.js
+++ data/default/projects.js
@@ -1,25 +1,77 @@
-export default {
+{
     "data": [
-        {
-            "id": 1,
-            "attributes": {
-                "Title": "Ecommerce Next 14js",
-                "Description": "A Fullstack Ecommerce With Mongo, Next, Node, Javascript.",
-                "Link": "https://www.upwork.com/freelancers/~01c67880a7f794eaa8",
-                "Image": "https://i.ytimg.com/vi/8xf78RNtfHY/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCjLZCjFLZbPMyz8shl-tdflJlL7g",
-                "createdAt": "2023-12-02T03:25:59.009Z",
-                "updatedAt": "2023-12-02T03:31:28.867Z",
-                "publishedAt": "2023-12-02T03:31:28.865Z",
-                "locale": "en"
-            }
+      {
+        "id": 1,
+        "attributes": {
+          "Title": "Ecommerce Next 14js",
+          "Description": "A Fullstack Ecommerce With Mongo, Next, Node, Javascript.",
+          "Link": "https://www.upwork.com/freelancers/~01c67880a7f794eaa8",
+          "Image": "https://i.ytimg.com/vi/8xf78RNtfHY/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCjLZCjFLZbPMyz8shl-tdflJlL7g",
+          "createdAt": "2023-12-02T03:25:59.009Z",
+          "updatedAt": "2023-12-02T03:31:28.867Z",
+          "publishedAt": "2023-12-02T03:31:28.865Z",
+          "locale": "en"
         }
+      },
+      {
+        "id": 2,
+        "attributes": {
+          "Title": "Whatsapp Chat Integration",
+          "Description": "A FullStack Whatsapp API Integration on WebPanel CRM",
+          "Link": "https://www.upwork.com/freelancers/~01c67880a7f794eaa8",
+          "Image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/640px-WhatsApp.svg.png",
+          "createdAt": "2023-12-02T03:25:59.009Z",
+          "updatedAt": "2023-12-02T03:31:28.867Z",
+          "publishedAt": "2023-12-02T03:31:28.865Z",
+          "locale": "en"
+        }
+      },
+      {
+        "id": 3,
+        "attributes": {
+          "Title": "Mercado Pago Payment",
+          "Description": "A FullStack MercadoPago Integration of Payments",
+          "Link": "https://www.upwork.com/freelancers/~01c67880a7f794eaa8",
+          "Image": "https://d2r9epyceweg5n.cloudfront.net/apps/5576-es_AR-Mercado%20Pago.jpg",
+          "createdAt": "2023-12-02T03:25:59.009Z",
+          "updatedAt": "2023-12-02T03:31:28.867Z",
+          "publishedAt": "2023-12-02T03:31:28.865Z",
+          "locale": "en"
+        }
+      },
+      {
+        "id": 3,
+        "attributes": {
+          "Title": "Paypal Payment",
+          "Description": "A FullStack Paypal Integration of Payments",
+          "Link": "https://www.upwork.com/freelancers/~01c67880a7f794eaa8",
+          "Image": "https://upload.wikimedia.org/wikipedia/commons/a/a4/Paypal_2014_logo.png",
+          "createdAt": "2023-12-02T03:25:59.009Z",
+          "updatedAt": "2023-12-02T03:31:28.867Z",
+          "publishedAt": "2023-12-02T03:31:28.865Z",
+          "locale": "en"
+        }
+      },
+      {
+        "id": 4,
+        "attributes": {
+          "Title": "Crypto Payment",
+          "Description": "A FullStack Crypto Integration of Payments",
+          "Link": "https://www.upwork.com/freelancers/~01c67880a7f794eaa8",
+          "Image": "https://w7.pngwing.com/pngs/1018/698/png-transparent-bitcoin-cash-cryptocurrency-money-blockchain-election-flyers-text-orange-logo.png",
+          "createdAt": "2023-12-02T03:25:59.009Z",
+          "updatedAt": "2023-12-02T03:31:28.867Z",
+          "publishedAt": "2023-12-02T03:31:28.865Z",
+          "locale": "en"
+        }
+      }
     ],
     "meta": {
-        "pagination": {
-            "page": 1,
-            "pageSize": 25,
-            "pageCount": 1,
-            "total": 1
-        }
+      "pagination": {
+        "page": 1,
+        "pageSize": 25,
+        "pageCount": 1,
+        "total": 4
+      }
     }
-}
+  }
