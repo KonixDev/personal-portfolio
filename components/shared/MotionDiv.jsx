65c80e9e3b5a89ebd9b5e2ae057d875d34c0b94f
Martin Coll <martincoll000@gmail.com>
2024-01-13 10:49:26 -0300

    Removed framer motion

--- components/shared/MotionDiv.jsx
+++ components/shared/MotionDiv.jsx
@@ -1,18 +1,18 @@
-"use client";
+// "use client";
 
-import React from "react";
-import { motion } from "framer-motion";
+// import React from "react";
+// import { motion } from "framer-motion";
 
 const MotionDiv = ({ children, ...props }) => {
   return (
-    <motion.div
-      whileInView={{ opacity: 1 }}
-      initial={{ opacity: 0 }}
-      transition={{ duration: 2 }}
-      {...props}
-    >
-      {children}
-    </motion.div>
+    // <motion.div
+    //   whileInView={{ opacity: 1 }}
+    //   initial={{ opacity: 0 }}
+    //   transition={{ duration: 2 }}
+    //   {...props}
+    // >
+    <div>{children}</div>
+    // </motion.div>
   );
 };
 
