911970f57ce5bf0760f93160f54d5e339649fbd5
Martin Coll <mcoll@toolboxtve.com>
2025-11-14 04:43:36 -0300

    feat: update next.config.ts and package.json for optimized image handling and build process

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true, // Requerido para next/image
-    domains: [
-      "images.unsplash.com",
-      "www.schoolofsciencery.com",
-      "picsum.photos",
-    ],
-  },
-  basePath: "/personal-portfolio", // Si tu repo NO es del tipo usuario/usuario.github.io
-  assetPrefix: "/personal-portfolio", // Igual que arriba
 };
 
 module.exports = nextConfig;
